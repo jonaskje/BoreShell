bb23b3ab261e251c5e9e96c394842483cf792eda
JonasKjellstrom <jonas.kjellstrom@embark-studios.com>
2019-11-18 23:37:52 +0100

    Upgrade to 7.0.0-preview5

--- Build.ps1
+++ Build.ps1
@@ -4,62 +4,63 @@
 [string]$BoreShellExe = "$BoreShellDir\BoreShell.exe"
 
 function InstallScoopApplications() {
-	scoop bucket add shelmangroup https://github.com/shelmangroup/scoops
-	scoop bucket add extras
-	scoop install oidc-agent
-	scoop install extras/vcredist2015
-	scoop install extras/vcredist2017
-	scoop install everything
-	scoop install neovim 
-	scoop install totalcommander
-	scoop install alacritty
-	scoop update *
+    scoop bucket add shelmangroup https://github.com/shelmangroup/scoops
+    scoop bucket add extras
+    scoop install oidc-agent
+    scoop install extras/vcredist2015
+    scoop install extras/vcredist2017
+    scoop install extras/vcredist2019
+    scoop install everything
+    #scoop install neovim 
+    scoop install totalcommander
+    #scoop install alacritty
+    scoop update *
 }
 
 function BuildBoreShell() {
-	$PSVersion = "6.2.0"
-	$PsGalleryModules = "PowerLine", "PSEverything", "ClipboardText", "WindowsCompatibility", "Ships", "ThreadJob", "ImportExcel", "posh-git", "vim"
-	$PsUri = "https://github.com/PowerShell/PowerShell/releases/download/v$PSVersion/PowerShell-$PSVersion-win-x64.zip"
+    $PSVersion = "7.0.0-preview.5"
+    $PsGalleryModules = "PowerLine", "PSEverything", "ClipboardText", "WindowsCompatibility", "Ships", "ThreadJob", "ImportExcel", "posh-git", "vim"
+    $PsUri = "https://github.com/PowerShell/PowerShell/releases/download/v$PSVersion/PowerShell-$PSVersion-win-x64.zip"
 
-	if (Test-Path ($BoreShellExe)){
-		$BoreShellTimeStamp = (Get-ChildItem $BoreShellExe).LastWriteTime
-		if ($BoreShellTimeStamp -ge $TimeStamp) {
-			Write-Host "Skipping BoreShell build because no changes were detected"
-			return
-		}
-	}
+    if (Test-Path ($BoreShellExe)) {
+        $BoreShellTimeStamp = (Get-ChildItem $BoreShellExe).LastWriteTime
+        if ($BoreShellTimeStamp -ge $TimeStamp) {
+            Write-Host "Skipping BoreShell build because no changes were detected"
+            return
+        }
+    }
 
-	if (Test-Path ($BoreShellDir)){
-		Remove-Item $BoreShellDir -Recurse -Force
-	}
+    if (Test-Path ($BoreShellDir)) {
+        Remove-Item $BoreShellDir -Recurse -Force
+    }
 
-	mkdir $BoreShellDir\Modules | out-null
+    mkdir $BoreShellDir\Modules | out-null
 
-	$localZip = "$RootDir\packages\$PSVersion-win-x64.zip"
-	if (!(Test-Path $localZip)){
-		mkdir (split-path $localZip) -ea:0
-		Write-Progress -activity "Publish BoreShell" -Status "Download powershell" -CurrentOperation $PsUri -PercentComplete 0 -Id 1
-		Invoke-WebRequest -Uri $PsUri -OutFile $localZip
-	}
+    $localZip = "$RootDir\packages\$PSVersion-win-x64.zip"
+    if (!(Test-Path $localZip)) {
+        mkdir (split-path $localZip) -ea:0
+        Write-Progress -activity "Publish BoreShell" -Status "Download powershell" -CurrentOperation $PsUri -PercentComplete 0 -Id 1
+        Invoke-WebRequest -Uri $PsUri -OutFile $localZip
+    }
 
-	Write-Progress -activity "Publish BoreShell" -Status "Expanding powershell" -CurrentOperation $localZip -PercentComplete 25 -Id 1
-	Expand-Archive -Path $localZip -DestinationPath $BoreShellDir
+    Write-Progress -activity "Publish BoreShell" -Status "Expanding powershell" -CurrentOperation $localZip -PercentComplete 25 -Id 1
+    Expand-Archive -Path $localZip -DestinationPath $BoreShellDir
 
-	Write-Progress -activity "Publish BoreShell" -Status "Fetching modules" -CurrentOperation "$PsGalleryModules" -PercentComplete 50 -Id 1
-	Save-Module -Repository PSGallery -Name $PsGalleryModules -LiteralPath $BoreShellDir\Modules
+    Write-Progress -activity "Publish BoreShell" -Status "Fetching modules" -CurrentOperation "$PsGalleryModules" -PercentComplete 50 -Id 1
+    Save-Module -Repository PSGallery -Name $PsGalleryModules -LiteralPath $BoreShellDir\Modules
 
-	Copy-Item $PSScriptRoot\Extra\* -Recurse -Destination $BoreShellDir\
+    Copy-Item $PSScriptRoot\Extra\* -Recurse -Destination $BoreShellDir\
 
-	Move-Item $BoreShellDir\pwsh.exe $BoreShellExe
-	(Get-ChildItem $BoreShellExe).LastWriteTime = Get-Date
+    Move-Item $BoreShellDir\pwsh.exe $BoreShellExe
+    (Get-ChildItem $BoreShellExe).LastWriteTime = Get-Date
 }
 
 & "$RootDir\Add-Font.ps1" -path "$RootDir\Extra\Fonts\"
 InstallScoopApplications
 BuildBoreShell
 
-mkdir "$env:APPDATA\alacritty" -ErrorAction SilentlyContinue
-(get-content "$RootDir\appdata\alacritty\alacritty.yml") -Replace "@@@BORESHELL@@@",$BoreShellExe | Out-File -Force -Encoding ascii -FilePath "$env:APPDATA\alacritty\alacritty.yml"
+# mkdir "$env:APPDATA\alacritty" -ErrorAction SilentlyContinue
+# (get-content "$RootDir\appdata\alacritty\alacritty.yml") -Replace "@@@BORESHELL@@@",$BoreShellExe | Out-File -Force -Encoding ascii -FilePath "$env:APPDATA\alacritty\alacritty.yml"
 
 cp -Force "$RootDir\scoop\persist\totalcommander\wincmd.ini" "$env:USERPROFILE\scoop\persist\totalcommander\wincmd.ini"
 
